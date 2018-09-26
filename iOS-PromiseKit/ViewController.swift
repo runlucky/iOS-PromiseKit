e3d28f4c8db4e2b31dd862a6b12cbb0019830375
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-26 11:17:10 +0900

    適当に使ってみる

--- iOS-PromiseKit/ViewController.swift
+++ iOS-PromiseKit/ViewController.swift
@@ -7,20 +7,40 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello, 🍄")
-        // Do any additional setup after loading the view, typically from a nib.
+
+        _ = Promise<String> { seal in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                seal.fulfill("DONE!")
+                print(1)
+            }
+        }.then { result -> Promise<String> in
+            Promise<String> { seal in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    print(2)
+                    print("result: " + result)
+                    seal.fulfill("DONE!!")
+                }
+            }
+        }.done { value in
+            print(3)
+            print(value)
+        }
+
+        print(4)
     }
+
+    // Do any additional setup after loading the view, typically from a nib.
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
