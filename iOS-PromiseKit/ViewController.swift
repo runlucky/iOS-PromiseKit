adcf8550d21821b5dc49814ac2a0df2f3dba12a7
Kakeru Fukuda <stand.by.we@gmail.com>
2018-09-26 10:26:22 +0900

    ガワ作成

---
+++ iOS-PromiseKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOS-PromiseKit
+//
+//  Created by H5266 on 2018/09/26.
+//  Copyright © 2018年 Kakeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello, 🍄")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS-PromiseKitTests/iOS_PromiseKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_PromiseKitTests.swift
+//  iOS-PromiseKitTests
+//
+//  Created by H5266 on 2018/09/26.
+//  Copyright © 2018年 Kakeru. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_PromiseKit
+
+class iOS_PromiseKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
